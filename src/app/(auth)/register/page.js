66cb54a57040cc42ae8e74a471491c842bbb00ab
Asad Khan <asadbhubaneswar@gmail.com>
2025-05-10 16:38:53 +0530

    Fix: Wrap RegisterForm in Suspense to support useSearchParams

--- src/app/(auth)/register/page.js
+++ src/app/(auth)/register/page.js
@@ -1,10 +1,13 @@
+import { Suspense } from 'react';
 import RegisterForm from '../../../components/RegisterForm';
 import { Toaster } from 'react-hot-toast';
 
 export default function Register() {
   return (
     <div>
-      <RegisterForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <RegisterForm />
+      </Suspense>
       <Toaster />
     </div>
   );
